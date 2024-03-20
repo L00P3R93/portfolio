e6f2e16b2fcf4d934d625ad960d49208edd1607c
Vincent Kioko <vincentkioko51@gmail.com>
2024-03-20 03:42:35 +0300

    vite typescript gitprofile configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+import CONFIG from './gitprofile.config'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+  daisyui: {
+    logs: false,
+    themes: [
+      ...CONFIG.themeConfig.themes,
+      { procyon: CONFIG.themeConfig.customTheme },
+    ],
+  },
+};
 
