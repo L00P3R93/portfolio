84edbc4dbee4a1ba01a3710d7d2a9193e38034f3
Vincent Kioko <vincentkioko51@gmail.com>
2024-03-20 04:16:00 +0300

    Updated defaultTheme - emerald

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -168,7 +168,7 @@ const CONFIG = {
 		snippetVersion: 6,
 	},
 	themeConfig: {
-		defaultTheme: 'lofi',
+		defaultTheme: 'emerald',
 
 		// Hides the switch in the navbar
 		// Useful if you want to support a single color mode
